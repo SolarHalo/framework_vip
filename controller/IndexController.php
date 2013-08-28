ce4d325aa763a60a392558e0439ba54401f80d37
superyuyue <superyuyue@126.com>
2013-08-28 22:40:51 +0800

    google anylze

--- controller/IndexController.php
+++ controller/IndexController.php
@@ -4,8 +4,13 @@ class IndexController extends  Controller{
 	
 	public function index(){ 
 	 $smaryt = $this->getSmarty(); 
-	  
-	 $this->smarty->display("index.tpl"); 
+	  require_once SERVICE.DS.'admin'.DS.'ParamService.class.php';
+	 
+		$params = 'google';
+		$paramService = new ParamService($this->getDB());
+		$parame = $paramService->getParamByname($params);
+		$this->smarty->assign("googleanylze",$parvalue);
+	    $this->smarty->display("index.tpl"); 
 	}
 	
 }
